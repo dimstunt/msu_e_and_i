82749879a81bcecf3f7feb9753f8a129d3dc2968
d.cherkashin <d.cherkashin@city-mobil.ru>
2020-05-23 18:37:52 +0300

    parser: update ConnectionManager
    * Причесал класс ConnectionManager

--- TorRequests/parser.py
+++ TorRequests/parser.py
@@ -9,12 +9,12 @@ cm = ConnectionManager()
 #         print(cm.request("http://icanhazip.com/").text.strip())
 
 r = cm.request('https://shikimori.one/animes/page/1')
-# html = BeautifulSoup(r.content, 'html.parser')
-# for el in html.select('.title'):
-#     title_en = el.select_one('.name-en').text
-#     title_ru = el.select_one('.name-ru')['data-text']
-#     href = ''.join(['https://shikimori.one', el['href']])
-#     print(title_en, title_ru, href)
+html = BeautifulSoup(r.content, 'html.parser')
+for el in html.select('.title'):
+    title_en = el.select_one('.name-en').text
+    title_ru = el.select_one('.name-ru')['data-text']
+    href = ''.join(['https://shikimori.one', el['href']])
+    print(title_en, title_ru, href)
 
 # for i in range(704)
 # https://shikimori.one/animes/page/
