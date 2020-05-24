e657d580a3fa41d8ef3bfa85c62ea4a501687e54
d.cherkashin <d.cherkashin@city-mobil.ru>
2020-05-24 10:17:00 +0300

    parser: кавычечки

--- ConnectionManager/__main__.py
+++ ConnectionManager/__main__.py
@@ -3,16 +3,16 @@ import logging
 
 
 def main():
-    module_logger = logging.getLogger("ConnectionManager")
+    module_logger = logging.getLogger('ConnectionManager')
     module_logger.setLevel(logging.INFO)
-    fh = logging.FileHandler("ConnectionManager.log")
+    fh = logging.FileHandler('ConnectionManager.log')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     module_logger.addHandler(fh)
-    module_logger.info(msg="Testing changing ip")
+    module_logger.info(msg='Testing changing ip')
     cm = ConnectionManager.ConnectionManager(3)
     for i in range(15):
-        module_logger.info(msg="{i} ip: {ip}".format(i=i, ip=cm.request("http://icanhazip.com/").text.strip()))
+        module_logger.info(msg='{i} ip: {ip}'.format(i=i, ip=cm.request("http://icanhazip.com/").text.strip()))
 
 
 if __name__ == '__main__':
--- ShikimoryParser/__main__.py
+++ ShikimoryParser/__main__.py
@@ -13,9 +13,9 @@ def main():
     anime_list = []
     bad_html_list = []
 
-    print("Парсим страницы со списком аниме")
+    print('Парсим страницы со списком аниме')
     for page_num in range(PAGE_NUM_FIRST, PAGE_NUM_LAST + 1):
-        print("Страница: {page_num} из {page_num_last}".format(page_num=page_num, page_num_last=PAGE_NUM_LAST))
+        print('Страница: {page_num} из {page_num_last}'.format(page_num=page_num, page_num_last=PAGE_NUM_LAST))
         res_html, res_json_list = shikimory_parser.parse_anime_list(page_num)
         bad_html_cnt = 0
         for res_json in res_json_list:
@@ -26,14 +26,14 @@ def main():
             if bad_html_cnt:
                 bad_html_list.append(res_html)
         if bad_html_cnt:
-            print("\tНа странице {page_num} нет ссылок на {bad_html_cnt} аниме"
+            print('\tНа странице {page_num} нет ссылок на {bad_html_cnt} аниме'
                   .format(page_num=page_num, bad_html_cnt=bad_html_cnt))
 
     len_anime = len(anime_list)
     len_bad_html = len(bad_html_list)
-    print(("Все страницы распаршены, всего аниме {len_anime} шт.\n"
-           "Нераспаршеных стианиц: {len_bad_html}"
-           "Сохранение выкаченных названий\n").format(len_bad_html=len_bad_html, len_anime=len_anime))
+    print(('Все страницы распаршены, всего аниме {len_anime} шт.\n'
+           'Нераспаршеных стианиц: {len_bad_html}'
+           'Сохранение выкаченных названий\n').format(len_bad_html=len_bad_html, len_anime=len_anime))
 
     keys = set()
     for anime in anime_list:
@@ -43,8 +43,8 @@ def main():
         dict_writer.writeheader()
         dict_writer.writerows(anime_list)
 
-    print("Выкаченные названия сохранены\n"
-          "Выкачка инфе о каждом тайтле\n")
+    print('Выкаченные названия сохранены\n'
+          'Выкачка инфе о каждом тайтле\n')
 
     for id_anime, anime in enumerate(anime_list):
         print('Аниме №{id_anime} из {len_anime}'.format(id_anime=id_anime + 1, len_anime=len_anime))
@@ -52,7 +52,7 @@ def main():
         # TODO обработать res_html
         anime.update(res_json)
 
-    print("сохранение всех аниме")
+    print('сохранение всех аниме')
     keys = set()
     for anime in anime_list:
         keys = keys.union(anime.keys())
@@ -60,7 +60,7 @@ def main():
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(anime_list)
-    print("все аниме сохранены")
+    print('все аниме сохранены')
 
 
 if __name__ == "__main__":
